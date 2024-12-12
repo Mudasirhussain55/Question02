94c5a29e1dce42a6c9f3ae942c17cbc0fbfce060
mudasirhussain55 <hmudasir972@gmail.com>
2024-12-13 03:01:38 +0500

    question02

---
+++ question02.dart
@@ -0,0 +1,16 @@
+void main (){
+age1 = 25
+age2 = 30
+if age1 > age2:
+    print("The oldest is", age1, "and the youngest is", age2)
+elif age1 < age2:
+    print("The oldest is", age2, "and the youngest is", age1)
+else:
+    print("Both are of the same age."
+
+
+
+
+
+
+}
